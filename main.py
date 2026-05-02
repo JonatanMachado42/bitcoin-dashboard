5eec691ddad92935312a352d311e2e27a2f4104f
jonatan leandro machado <jonatanleandromachado@proton.me>
2026-05-02 13:16:18 -0300

    add more metrics

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 
 from src.data_loader import data_loader
 from src.data_cleaner import data_cleaner
-
+from src.metrics import metrics
+from src.metrics import min_change, min_change_porcentage, daily_change, weekly_change, monthly_change
 
 dir_path = kagglehub.dataset_download("mczielinski/bitcoin-historical-data")
 full_path = os.path.join(dir_path, "btcusd_1-min_data.csv")
@@ -12,9 +13,14 @@ full_path = os.path.join(dir_path, "btcusd_1-min_data.csv")
 if __name__ == '__main__':
     dataset = data_loader(full_path)
     dataset = data_cleaner(dataset)
-    
+    dataset = min_change(dataset)
+    dataset = min_change_porcentage(dataset)
+    dataset = daily_change(dataset)
 
-    
+    print(metrics(dataset))
+    print(dataset.tail())
+
+
 
 
     
---
+++ src/metrics.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+
+def metrics(dataframe: pd.DataFrame) -> pd.DataFrame:
+    today_value = dataframe["Close"].iloc[-1]
+    max_value = dataframe["Close"].max()
+    min_value = dataframe["Close"].min()
+    avg_value = dataframe["Close"].mean()
+    volatility = dataframe["Close"].std()
+
+    print(f"Today's value: ${today_value}")
+    print(f"Max value: ${max_value}")
+    print(f"Min value: ${min_value}")
+    print(f"Average value: ${avg_value}")
+    print(f"Volatility: ${volatility}")
+
+    return today_value, max_value, min_value, avg_value, volatility
+
+
+def min_change(dataframe: pd.DataFrame) -> pd.DataFrame:
+    dataframe["Change"] = dataframe["Close"].diff()
+    return dataframe
+
+
+def min_change_porcentage(dataframe: pd.DataFrame) -> pd.DataFrame:
+    dataframe["Change_porcentage"] = dataframe["Close"].pct_change() * 100
+    return dataframe
+
+
+
+def daily_change(dataframe: pd.DataFrame) -> pd.DataFrame:
+    dataframe["Change_daily"] = dataframe["Close"] - dataframe["Close"].shift(1440)
+    return dataframe
+
+def weekly_change(dataframe: pd.DataFrame) -> pd.DataFrame:
+    dataframe["Change_weekly"] = dataframe["Close"] - dataframe["Close"].shift(10080)
+    return dataframe
+
+
+
+def monthly_change(dataframe: pd.DataFrame) -> pd.DataFrame:
+    dataframe["Change_monthly"] = dataframe["Close"] - dataframe["Close"].shift(43200)
+    return dataframe
+
+
+def rolling_avg_weekly(dataframe: pd.DataFrame) -> pd.DataFrame:
+    dataframe["rolling_avg_weekly"] = dataframe["Close"].rolling(window=10080).mean()
+    return dataframe
+
+
+def rolling_avg_monthly(dataframe: pd.DataFrame) -> pd.DataFrame:
+    dataframe["rolling_avg_monthly"] = dataframe["Close"].rolling(window=43200).mean()
+    return dataframe
+
